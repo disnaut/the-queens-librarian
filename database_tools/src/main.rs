9cf60443356905f58ad544dfa4317186fbd01830
disnaut <damienr2017@gmail.com>
2023-02-19 20:35:13 -0800

    Removal of Rust in favor of Go

--- database_tools/src/main.rs
+++
@@ -1,8 +0,0 @@
-mod database_creation;
-mod database_insertion;
-
-fn main() {
-    println!("Hello, world!");
-    database_creation::database_initialization::initialize();
-    database_insertion::database_insertion::read_file();
-}
