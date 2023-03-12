11e1366776c3540246778d4110ea62829fb09dca
disnaut <damienr2017@gmail.com>
2023-03-11 20:32:52 -0800

    Updated controller w/ struct and seq. rqsts

--- controllers/card.go
+++ controllers/card.go
@@ -1,16 +1,20 @@
 package controllers
 
+/*
+TODO: Get manacost up and running
+TODO: Change out query param variables to pointers
+TODO: Update the response to multiple http requests one after another rather than array
+*/
+
 import (
 	"context"
 	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
 	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type CardsController struct {
@@ -19,6 +23,17 @@ type CardsController struct {
 
 func NewCardsController(collection *mongo.Collection) *CardsController {
 	return &CardsController{collection}
+}
+
+type CardQueryParams struct {
+	name     string
+	colors   []string
+	cardType string
+	artist   string
+	keywords []string
+	set      string
+	mana     string
+	rarity   string
 }
 
 func (cc *CardsController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -56,15 +71,16 @@ Would be worth looking into getting requests one at a time.
 */
 func (cc *CardsController) SearchCards(w http.ResponseWriter, r *http.Request) {
 	/* region: Grabbing Query Parameters */
-	name, colors_arr, types, artist, keywords_arr, set, manacost, rarity, page, pageSize := GetQueryParams(r)
+	var cardQuery CardQueryParams
 
-	println(manacost) //To keep manacost available while options are considered for what can be done.
+	GetQueryParams(r, &cardQuery)
+
 	var query_collection []bson.M
 	//Create a regex based on certain params
-	name_pattern := bson.M{"$regex": name, "$options": "i"}
-	artist_pattern := bson.M{"$regex": artist, "$options": "i"}
-	set_pattern := bson.M{"$regex": set, "$options": "i"}
-	types_pattern := bson.M{"$regex": types, "$options": "i"}
+	name_pattern := bson.M{"$regex": cardQuery.name, "$options": "i"}
+	artist_pattern := bson.M{"$regex": cardQuery.artist, "$options": "i"}
+	set_pattern := bson.M{"$regex": cardQuery.set, "$options": "i"}
+	types_pattern := bson.M{"$regex": cardQuery.cardType, "$options": "i"} //Change to regular query, something like only Artifacts. No solution for if multiple types are wanted in the query
 
 	//assign regex patterns to respective queries
 	name_query := bson.M{"name": name_pattern}
@@ -73,30 +89,28 @@ func (cc *CardsController) SearchCards(w http.ResponseWriter, r *http.Request) {
 	types_query := bson.M{"type_line": types_pattern}
 
 	query_collection = append(query_collection, name_query, artist_query, set_query, types_query)
-	/* endregion */
 
 	/* $and setup for non regex types */
-	if len(colors_arr) != 0 {
-		colors_query := bson.M{"colors": colors_arr}
+	if len(cardQuery.colors) != 0 {
+		colors_query := bson.M{"colors": cardQuery.colors}
 		query_collection = append(query_collection, colors_query)
 	}
 
-	if len(keywords_arr) != 0 {
-		keywords_query := bson.M{"keywords": keywords_arr}
+	if len(cardQuery.keywords) != 0 {
+		keywords_query := bson.M{"keywords": cardQuery.keywords}
 		query_collection = append(query_collection, keywords_query)
 	}
 
-	if len(rarity) != 0 {
-		rarity_query := bson.M{"rarity": rarity}
+	if len(cardQuery.rarity) != 0 {
+		rarity_query := bson.M{"rarity": cardQuery.rarity}
 		query_collection = append(query_collection, rarity_query)
 	}
 
-	query := bson.M{"$and": query_collection}
+	query := bson.M{"$and": query_collection} //Potentially figure out a way to handle an or statement.w
 	//Calculate the number of documents to skip based on the page number and page
-	skip := (page - 1) * pageSize
 
 	//Query cards collection with a limit and skip
-	cursor, err := cc.collection.Find(context.Background(), query, options.Find().SetLimit(int64(pageSize)).SetSkip(int64(skip)))
+	cursor, err := cc.collection.Find(context.Background(), query)
 	if err != nil {
 		log.Fatal("Error occured while getting cards from collection.")
 		log.Fatal(err)
@@ -104,83 +118,41 @@ func (cc *CardsController) SearchCards(w http.ResponseWriter, r *http.Request) {
 	defer cursor.Close(context.Background())
 
 	//Iterate over the results and write to the response
-	var cards []bson.M
+	var card bson.M
 	for cursor.Next(context.Background()) {
-		var card bson.M
 		err := cursor.Decode(&card)
 		if err != nil {
 			log.Fatal("Error occured while grabbing card.")
 			log.Fatal(err)
 		}
-		cards = append(cards, card)
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(card)
+		w.(http.Flusher).Flush()
 	}
 	if err := cursor.Err(); err != nil {
 		log.Fatal(err) //Construct and query
-
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(cards)
 }
 
-/*
-Parameters that we can get from this:
-  - Name => String regex [X]
-  - Color => []string [X]
-  - Type => []string [X]
-  - Artist => string regex [X]
-  - Keywords => []string [X]
-  - Set => name regex [X]
-  - Manacost => using cmc, which is a number.
-  - Rarity => string [X]
-    artist := r.URL.Query().Get("artist")
-    set := r.URL.Query().Get("set")
-    rarity := r.URL.Query().Get("rarity")
-    types := r.URL.Query().Get("type")
-  - page
-  - pagesize
-*/
-func GetQueryParams(r *http.Request) (string, []string, string, string, []string, string, int, string, int, int) {
+func GetQueryParams(r *http.Request, cardPtr *CardQueryParams) {
 	/* Strings */
-	name := r.URL.Query().Get("name")
-	artist := r.URL.Query().Get("artist")
-	set := r.URL.Query().Get("set")
-	rarity := r.URL.Query().Get("rarity")
-	types := r.URL.Query().Get("types")
+	*&cardPtr.name = r.URL.Query().Get("name")
+	*&cardPtr.artist = r.URL.Query().Get("artist")
+	*&cardPtr.set = r.URL.Query().Get("set")
+	*&cardPtr.rarity = r.URL.Query().Get("rarity")
+	*&cardPtr.cardType = r.URL.Query().Get("types")
+	*&cardPtr.mana = r.URL.Query().Get("manaCost")
 
 	/* Arrays */
 	colors := r.URL.Query().Get("colors")
 	keywords := r.URL.Query().Get("keywords")
 
-	var colors_arr []string
-	var keywords_arr []string
-
 	if len(colors) != 0 {
-		colors_arr = strings.Split(colors, ",")
+		*&cardPtr.colors = strings.Split(colors, ",")
 	}
 
 	if len(keywords) != 0 {
-		keywords_arr = strings.Split(keywords, ",")
+		*&cardPtr.keywords = strings.Split(keywords, ",")
 	}
-
-	/* Numbers */
-	// This can be moved up, because we need to split the comparison
-	// from the number so we know which way we need to grab certain
-	// things.
-	manacost, err := strconv.Atoi(r.URL.Query().Get("manacost"))
-	if err != nil {
-		manacost = -1
-	}
-
-	page, err := strconv.Atoi(r.URL.Query().Get("page"))
-	if err != nil {
-		page = 1 //default to page 1 if the query parsing breaks.
-	}
-
-	pageSize, err := strconv.Atoi(r.URL.Query().Get("pagesize"))
-	if err != nil {
-		pageSize = 10 //default to size 10 if the query parsing breaks
-	}
-
-	return name, colors_arr, types, artist, keywords_arr, set, manacost, rarity, page, pageSize
 }
